c45e212e6a996b6de243768289801cc62814633d
davidoises <davidoises@hotmail.com>
2024-11-11 00:58:03 -0800

    Initial changes to switch to ROS1

---
+++ env_setup_david/ros1/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update and install necessary packages
+sudo apt update
+
+sudo apt install nano -y
+sudo apt install python3-pip
+
+#https://docs.clearpathrobotics.com/docs/ros1noetic/robots/outdoor_robots/jackal/tutorials_jackal/
+sudo apt-get install -y ros-noetic-jackal-simulator ros-noetic-jackal-desktop ros-noetic-jackal-navigation
+
+# # Source the ROS 2 setup script
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Create a workspace
+# mkdir -p ~/ros2_ws/src
+# cd ~/ros2_ws
+
+# colcon build
+# chmod +x UrbanFireRobot/ros2_ws/src/warehouse_simulation/scripts/warehouse_generator.py
