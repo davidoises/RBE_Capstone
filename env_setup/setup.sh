ede75928a3c03c8449182cb17ae606f74265b817
Sean <smberta30@gmail.com>
2024-11-19 20:58:26 -0600

    fixed emitteer dependency

--- env_setup/setup.sh
+++ env_setup/setup.sh
@@ -11,11 +11,18 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -cs) main
 
 # Update and install ROS 1 Noetic
 sudo apt update && sudo apt install -y ros-noetic-desktop-full
-sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-rosdep pytohn3-pip python3-vcstool
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-rosdep pytohn3-pip python3-vcstool python3-colcon-common-extensions
 
 #https://docs.clearpathrobotics.com/docs/ros1noetic/robots/outdoor_robots/jackal/tutorials_jackal/
 sudo apt-get install -y ros-noetic-jackal-simulator ros-noetic-jackal-desktop ros-noetic-jackal-navigation
 # Source the ROS 1 setup script
+sudo apt-get update
+sudo apt-get install lsb-release wget gnupg -y
+
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install -y ignition-edifice
 
 sudo apt-get install -y ros-noetic-ros-ign-gazebo
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
@@ -30,13 +37,13 @@ rosdep install --from-paths src --ignore-src --rosdistro noetic -y
 source ~/UrbanFireRobot/ros2_ws/install/setup.bash
 
 chmod +x UrbanFireRobot/ros2_ws/src/warehouse_simulation/scripts/warehouse_generator.py
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/UrbanFireRobot/ros2_ws/install/warehouse_simulation/share/warehouse_simulation/models
+# export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/UrbanFireRobot/ros2_ws/install/warehouse_simulation/share/warehouse_simulation/models
 export IGN_GAZEBO_RESOURCE_PATH=$IGN_GAZEBO_RESOURCE_PATH:~/UrbanFireRobot/ros2_ws/install/warehouse_simulation/share/warehouse_simulation/models
-export IGN_CONFIG_PATH=/usr/share/ignition
-export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/ign-gazebo-3/system_plugins
-export IGN_GAZEBO_RESOURCE_PATH=$IGN_GAZEBO_RESOURCE_PATH:$HOME/.ignition/fuel/fuel.ignitionrobotics.org/openrobotics/models
+# export IGN_CONFIG_PATH=/usr/share/ignition
+# export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/ign-gazebo-3/system_plugins
+# export IGN_GAZEBO_RESOURCE_PATH=$IGN_GAZEBO_RESOURCE_PATH:$HOME/.ignition/fuel/fuel.ignitionrobotics.org/openrobotics/models
 
 
 
-export LIBGL_ALWAYS_SOFTWARE=true
+# export LIBGL_ALWAYS_SOFTWARE=true
 
